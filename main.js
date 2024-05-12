b6a489ac08a00b0f3a3bc9b2ab584485d117912c
mrk-hnr <mrk.hnr@hotmail.com>
2024-05-12 17:29:46 +0800

    Cleaned up code, added function for Accordion sect

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const accordComplete = document.querySelectorAll(".accordComplete")
+const accordion = document.querySelectorAll(".accordion")
+const accordDesc = document.querySelectorAll(".accordDesc")
+
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -11,25 +15,51 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   navMenu.classList.remove("active");
 }))
 
+
+
+// TOGGLE FUNCTION for ACCORDION
+function newToggle() {
+  for (let i = 0; accordComplete.length; i++) {
+    accordComplete[i].addEventListener("click", toggleActive)
+    accordion[i].addEventListener("click", toggleActive)
+    accordDesc[i].addEventListener("click", toggleActive)
+  }
+}
+
+function toggleActive() {
+  this.classList.toggle("active")
+  this.classList.toggle("")
+}
+
+newToggle()
+
+// FONTAWEOME CHANGE FOR ACCORDION
+function changeClass(elem) {
+  var i = elem.children[1];
+  var c = i.classList;
+
+  // Change class
+  if (c.contains("fa-minus")) {
+      i.classList.remove("fa-minus");
+      i.classList.add("fa-plus");
+  } else if (c.contains("fa-plus")) {
+      i.classList.remove("fa-plus");
+      i.classList.add("fa-minus");
+  }
+}
+
+// accordComplete.addEventListener('click', () => {
+//   if (accordComplete.classList.contains("active")) {
+//     accordion.classList.toggle("active")
+//     accordDesc.classList.toggle("active")
+//   } else {
+//     accordComplete.classList.add("active")
+//   }
+// })
+
+
+
+
 // Displays year for Footer
 document.querySelector("#year").textContent = new Date().getFullYear();
 
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".rightNav");
-
-// hamburger.addEventListener("click", () => {
-//   hamburger.classList.toggle("active");
-//   navMenu.classList.toggle("active");
-// })
-
-// document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => {
-//   hamburger.classList.remove("active");
-//   navMenu.classList.remove("active");
-// }))
-
-// window.addEventListener('resize', e => {
-//     if (window.matchMedia(`(min-width: 750px)`).matches) {
-//         navMenu.classList.remove('active');
-//         hamburger.classList.remove('active');
-//     }
-//  });
